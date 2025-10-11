5934fc8bebe6575a08b734d06eaf3bb0ac502e42
egorsivkov <egorsivkov007@gmail.com>
2025-10-11 20:54:07 +0300

    The calculator and tests ready

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,31 @@
 import unittest
+from src.lab1.calculator import calculate
 
 class CalculatorTestCase(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
     def test_one(self):
-        self.assertEquals(1, 1)
+        self.assertEqual(calculate("2+2"), 4)
+
+    def test_two(self):
+        self.assertEqual(calculate("10-5"), 5)
+
+    def test_three(self):
+        self.assertEqual(calculate("3*4"), 12)
+
+    def test_four(self):
+        self.assertEqual(calculate("8/2"), 4)
+
+    def test_five(self):
+        self.assertEqual(calculate("5/0"), "Ошибка: деление на ноль!")
+
+    def test_six(self):
+        self.assertEqual(calculate("2^2"), "Ошибка: операция не распознана!")
+
+    def test_seven(self):
+        self.assertEqual(calculate("5"), "Ошибка: операция не распознана!")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
